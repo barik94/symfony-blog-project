485cf612c9686d1150948b4015f9c7f53e48beb4
barik94 <basya.den@mail.ru>
2014-03-23 23:11:13 +0200

    frontend completed

---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BARIK
+ * Date: 07.03.14
+ * Time: 18:17
+ */
+
+// src/Blogger/BlogBundle/Controller/PageController.php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()
+            ->getEntityManager();
+
+        $blogs = $em->getRepository('BloggerBlogBundle:Blog')
+            ->getLatestBlogs();
+
+        return $this->render('BloggerBlogBundle:Page:index.html.twig', array(
+            'blogs' => $blogs
+        ));
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->set('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function sidebarAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $tags = $em->getRepository('BloggerBlogBundle:Blog')->getTags();
+
+        $tagWeights = $em->getRepository('BloggerBlogBundle:Blog')->getTagWeights($tags);
+
+        $commentLimit = $this->container ->getParameter('blogger_blog.comments.latest_comment_limit');
+
+        $latestComments = $em->getRepository('BloggerBlogBundle:Comment')->getLatestComments($commentLimit);
+
+
+        return $this->render('BloggerBlogBundle:Page:sidebar.html.twig', array(
+            'latestComments'    => $latestComments,
+            'tags'              => $tagWeights
+        ));
+    }
+}
